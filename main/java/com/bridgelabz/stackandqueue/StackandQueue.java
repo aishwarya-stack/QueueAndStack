b52d954a213b1ee7c8524a993f840d93c6d96858
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-06 15:57:54 +0530

    ADD:welcome message

---
+++ main/java/com/bridgelabz/stackandqueue/StackandQueue.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stackandqueue;
+
+
+public class StackandQueue 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to stack and queue program" );
+    }
+}
---
+++ test/java/com/bridgelabz/stackandqueue/StackandqueueTest.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.stackandqueue;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class StackandqueueTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public StackandqueueTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( StackandqueueTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
